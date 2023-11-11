731ba2b5454922501a9053bfd0d4a8a58b7e6309
hickot <bydouglascontato@gmail.com>
2023-11-11 13:02:28 -0300

    Implementations Produto e Fornecedor.

---
+++ src/main/java/br/com/cotiinformatica/domain/services/CategoriaServiceImpl.java
@@ -0,0 +1,25 @@
+package br.com.cotiinformatica.domain.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.cotiinformatica.domain.entities.Categoria;
+import br.com.cotiinformatica.domain.interfaces.CategoriaService;
+import br.com.cotiinformatica.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaServiceImpl implements CategoriaService {
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
+	
+	@Override
+	public List<Categoria> findAll() throws Exception {
+		
+		//retornar todas as categorias cadastradas
+		return (List<Categoria>) categoriaRepository.findAll();
+	}
+
+}
