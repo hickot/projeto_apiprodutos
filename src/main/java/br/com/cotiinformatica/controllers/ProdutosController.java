6320cc681f2a8da0892da575982197d09d6e076d
hickot <bydouglascontato@gmail.com>
2023-11-18 12:58:27 -0300

    increment to the Produtos api.

--- src/main/java/br/com/cotiinformatica/controllers/ProdutosController.java
+++ src/main/java/br/com/cotiinformatica/controllers/ProdutosController.java
@@ -1,19 +1,38 @@
 package br.com.cotiinformatica.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cotiinformatica.domain.interfaces.ProdutoService;
+import br.com.cotiinformatica.dtos.ProdutoPostDto;
+import br.com.cotiinformatica.dtos.SuccessResponseDto;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "api/produtos")
 public class ProdutosController {
 	
+	@Autowired
+	ProdutoService produtoService;
+	
 	@PostMapping
-	public void post() {
-		//TODO
+	public ResponseEntity<SuccessResponseDto> post(@RequestBody @Valid ProdutoPostDto dto) throws Exception {
+		produtoService.create(dto);
+		
+		SuccessResponseDto responseDto = new SuccessResponseDto();
+		responseDto.setStatus(HttpStatus.CREATED);
+		responseDto.setMessage("Produto cadastrado com sucesso.");
+		
+		//devolvendo a resposta
+		return ResponseEntity.status(201).body(responseDto);
 	}
 	
 	@PutMapping
--- src/main/java/br/com/cotiinformatica/domain/services/ProdutoServiceImpl.java
+++ src/main/java/br/com/cotiinformatica/domain/services/ProdutoServiceImpl.java
@@ -1,22 +1,66 @@
 package br.com.cotiinformatica.domain.services;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.cotiinformatica.domain.entities.Categoria;
+import br.com.cotiinformatica.domain.entities.Fornecedor;
 import br.com.cotiinformatica.domain.entities.Produto;
 import br.com.cotiinformatica.domain.interfaces.ProdutoService;
+import br.com.cotiinformatica.dtos.ProdutoPostDto;
+import br.com.cotiinformatica.repositories.CategoriaRepository;
+import br.com.cotiinformatica.repositories.FornecedorRepository;
 import br.com.cotiinformatica.repositories.ProdutoRepository;
 
 @Service
 public class ProdutoServiceImpl implements ProdutoService {
 	@Autowired
 	ProdutoRepository produtoRepository;
+	
+	@Autowired
+	FornecedorRepository fornecedorRepository;
+	
+	@Autowired
+	CategoriaRepository categoriaRepository;
 
 	@Override
-	public void create(Produto produto) throws Exception {
+	public void create(ProdutoPostDto dto) throws Exception {
+		
+		//verificar se o fornecedor informado existe no banco de dados
+		Optional<Fornecedor> fornecedor = fornecedorRepository.findById(dto.getIdFornecedor());
+		
+		//buscando a categoria no banco de dados
+		Optional<Categoria> categoria = categoriaRepository.findById(dto.getIdCategoria());
+		
+		//verificando se o forncedor não foi encontrado
+		if(fornecedor.isEmpty()) {
+			
+			throw new IllegalArgumentException("Erro. O fornecedor informado não existe");
+		}
+		
+		//verificando se a categoria não foi encontrado
+		if(categoria.isEmpty()) {
+			
+			throw new IllegalArgumentException("Erro. A categoria informada não existe");
+		}
+		
+		//preencher os dados do produto
+		Produto produto = new Produto();
+		
+		produto.setId(UUID.randomUUID());
+		produto.setNome(dto.getNome());
+		produto.setPreco(new BigDecimal(dto.getPreco()));
+		produto.setQuantidade(dto.getQuantidade());
+		produto.setDescricao(dto.getDescricao());
+		produto.setFornecedor(fornecedor.get());
+		produto.setCategoria(categoria.get());
+		
+		//gravar no banco de dados
 		produtoRepository.save(produto);
 	}
 
